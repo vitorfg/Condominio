0c3285f2b55f1cc0a7d10851f4267e3931c43c8f
Vitor Garcia <vitorfgarcia@outlook.com>
2016-06-11 22:08:20 -0300

    Alteração na classe CondominioController

--- Condominio/src/main/java/br/com/fatec/controller/CondominioController.java
+++ Condominio/src/main/java/br/com/fatec/controller/CondominioController.java
@@ -1,5 +1,49 @@
 package br.com.fatec.controller;
 
+import javax.inject.Inject;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Post;
+import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.interceptor.IncludeParameters;
+import br.com.caelum.vraptor.validator.Validator;
+import br.com.fatec.dao.ApartamentoDao;
+import br.com.fatec.dao.CondominioDao;
+import br.com.fatec.model.Apartamento;
+import br.com.fatec.model.Condominio;
+
+@Controller
 public class CondominioController {
 
+	private CondominioDao condominioDao;
+	private Validator validator;
+	private Result result;
+	
+	
+	
+	public CondominioController() {
+	}
+
+
+	@Inject
+	public CondominioController(CondominioDao condominioDao, Validator validator, Result result) {
+		this.condominioDao = condominioDao;
+		this.validator = validator;
+		this.result = result;
+	}
+
+
+
+	public void form (){}
+	
+	
+	@IncludeParameters
+	@Post
+	public void adiciona(Condominio condominio){
+		validator.onErrorForwardTo(this).form(); //caso der erro fica na tela que está
+		//condominioDao.adiciona(condominio); // adiciona
+		//result.redirectTo(this).lista(); // redireciona para a lista
+	}
+	
+	
 }
