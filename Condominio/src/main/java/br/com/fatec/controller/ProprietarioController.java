149306ca5150a89720777c37b682927019f7e67b
Guilherme Vasconcellos <guiyllw@hotmail.com>
2016-06-11 21:32:38 -0300

    Alterações nas classes ProprietarioController.java e ProprietarioDao.java

--- Condominio/src/main/java/br/com/fatec/controller/ProprietarioController.java
+++ Condominio/src/main/java/br/com/fatec/controller/ProprietarioController.java
@@ -1,5 +1,38 @@
 package br.com.fatec.controller;
 
+import javax.inject.Inject;
+
+import br.com.caelum.vraptor.Post;
+import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.interceptor.IncludeParameters;
+import br.com.caelum.vraptor.validator.Validator;
+import br.com.fatec.dao.ProprietarioDao;
+import br.com.fatec.model.Proprietario;
+
 public class ProprietarioController {
 
+	private ProprietarioDao proprietarioDao;
+	private Validator validator;
+	private Result result;
+
+	public ProprietarioController() {
+	}
+
+	@Inject
+	ProprietarioController(ProprietarioDao proprietarioDao, Validator validator, Result result) {
+		this.proprietarioDao = proprietarioDao;
+		this.validator = validator;
+		this.result = result;
+	}
+
+	public void form() {
+	}
+
+	@IncludeParameters
+	@Post
+	public void adiciona(Proprietario proprietario) {
+		validator.onErrorForwardTo(this).form();
+		// apartamentoDao.adiciona(proprietario);
+		// result.redirectTo(this).lista();
+	}
 }
--- Condominio/src/main/java/br/com/fatec/dao/ProprietarioDao.java
+++ Condominio/src/main/java/br/com/fatec/dao/ProprietarioDao.java
@@ -1,5 +1,36 @@
 package br.com.fatec.dao;
 
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+import br.com.fatec.model.Proprietario;
+
 public class ProprietarioDao {
 
+	private EntityManager manager;
+
+	public ProprietarioDao() {
+
+	}
+
+	@Inject
+	public ProprietarioDao(EntityManager manager) {
+		this.manager = manager;
+	}
+
+	public void adiciona(Proprietario proprietario) {
+		if (proprietario.getId() == 0) {
+			manager.persist(proprietario);
+		} else {
+			manager.merge(proprietario);
+		}
+	}
+
+	public Proprietario busca(long id) {
+		return manager.find(Proprietario.class, id);
+	}
+
+	public void deleta(long id) {
+		manager.remove(manager.getReference(Proprietario.class, id));
+	}
 }
