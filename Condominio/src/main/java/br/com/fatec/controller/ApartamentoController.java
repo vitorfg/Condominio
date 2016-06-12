5e3b4d676e0a84cc3a87da4d26ec7532f1fa8526
Vitor Garcia <vitorfgarcia@outlook.com>
2016-06-11 21:35:01 -0300

    Alterações nas classes ApartamentoController.java e ApartamentoDao.java

--- Condominio/src/main/java/br/com/fatec/controller/ApartamentoController.java
+++ Condominio/src/main/java/br/com/fatec/controller/ApartamentoController.java
@@ -1,5 +1,47 @@
 package br.com.fatec.controller;
 
+import javax.inject.Inject;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Post;
+import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.interceptor.IncludeParameters;
+import br.com.caelum.vraptor.validator.Validator;
+import br.com.fatec.dao.ApartamentoDao;
+import br.com.fatec.model.Apartamento;
+
+@Controller
 public class ApartamentoController {
 
+	private ApartamentoDao apartamentoDao;
+	private Validator validator;
+	private Result result;
+	
+	
+	
+	public ApartamentoController() {
+	}
+
+
+	@Inject
+	public ApartamentoController(ApartamentoDao apartamentoDao, Validator validator, Result result) {
+		this.apartamentoDao = apartamentoDao;
+		this.validator = validator;
+		this.result = result;
+	}
+
+
+
+	public void form (){}
+	
+	
+	@IncludeParameters
+	@Post
+	public void adiciona(Apartamento apartamento){
+		validator.onErrorForwardTo(this).form(); //caso der erro fica na tela que está
+		apartamentoDao.adiciona(apartamento); // adiciona
+		//result.redirectTo(this).lista(); // redireciona para a lista
+	}
+	
+	
 }
--- Condominio/src/main/java/br/com/fatec/dao/ApartamentoDao.java
+++ Condominio/src/main/java/br/com/fatec/dao/ApartamentoDao.java
@@ -1,5 +1,44 @@
 package br.com.fatec.dao;
 
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+import br.com.fatec.model.Apartamento;
+
 public class ApartamentoDao {
 
+	private EntityManager manager;
+
+	public ApartamentoDao() {
+
+	}
+
+	@Inject
+	public ApartamentoDao(EntityManager manager) {
+		this.manager = manager;
+	}
+	
+	public List<Apartamento> lista(){
+		//terminar metodo
+		return null;
+	}
+	
+	public void adiciona(Apartamento apartamento){ // se não possuir id insere  no banco, se possuir id altera
+		if(apartamento.getId() == 0){
+			manager.persist(apartamento);
+		} else {
+			manager.merge(apartamento);
+		}
+	}
+	
+	public Apartamento busca(long id){ // busca pelo id
+		return manager.find(Apartamento.class, id);
+	}
+	
+	public void deleta (long id){ // deleta pelo id
+		manager.remove(manager.getReference(Apartamento.class, id));
+	}
+	
 }
